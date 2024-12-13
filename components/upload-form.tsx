49d1e9f683443d5005e36797ccfff083a8fc3682
AkashKumar-0 <ahmvaad@gmail.com>
2024-12-14 04:06:25 +0530

    removed toast

--- components/upload-form.tsx
+++ components/upload-form.tsx
@@ -8,50 +8,38 @@ import { UploadProgress } from "./upload-progress";
 import { UploadResponse } from "@/lib/types";
 import { useTransactions } from "@/hooks/store";
 import TransactionsList from "./transaction";
-import { useToast } from "@/hooks/use-toast";
+
 export function UploadForm() {
   const [isUploading, setIsUploading] = useState(false);
 
   const [extractedText, setExtractedText] = useState<string>("");
-  const { toast } = useToast();
   const { setTransactions } = useTransactions();
 
-  const handleDrop = useCallback(
-    async (acceptedFiles: File[]) => {
-      const file = acceptedFiles[0];
-      if (!file) return;
+  const handleDrop = useCallback(async (acceptedFiles: File[]) => {
+    const file = acceptedFiles[0];
+    if (!file) return;
 
-      if (!file.type.includes("pdf")) {
-        toast({
-          title: "Invalid file type",
-          description: "Please upload a PDF file",
-          variant: "destructive",
-        });
-        return;
-      }
+    if (!file.type.includes("pdf")) {
+      console.log({
+        title: "Invalid file type",
+        description: "Please upload a PDF file",
+        variant: "destructive",
+      });
+      return;
+    }
 
-      setIsUploading(true);
+    setIsUploading(true);
 
-      try {
-        const data = await uploadPdf(file);
-        console.log("hit", data);
-        setTransactions(data);
-        // setExtractedText(data);
-        toast({
-          description: "PDF uploaded and processed successfully",
-        });
-      } catch (error) {
-        toast({
-          description: "Failed to process PDF",
-          variant: "destructive",
-        });
-        console.log(error);
-      } finally {
-        setIsUploading(false);
-      }
-    },
-    [toast],
-  );
+    try {
+      const data = await uploadPdf(file);
+      console.log("hit", data);
+      setTransactions(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsUploading(false);
+    }
+  }, []);
 
   return (
     <div className="space-y-6 w-full">
